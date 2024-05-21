d71b47016d21edae0a04a12b8d02025c7b7a9c2b
drafish <xwlyy1991@163.com>
2024-05-21 09:40:16 +0800

    rm custom css classnames from LoadingScreen

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import BounceLoader from 'react-spinners/BounceLoader';
 import { useAppSelector } from '../../redux/hooks';
-import './index.css';
 
 const LoadingScreen: React.FC = () => {
   const loading = useAppSelector((state) => state.loading.screen);
 
   return loading ? (
-    <div className="spinnersOverlay">
-      <BounceLoader color="#a7b0ae" size={100} className="spinnersLoading" />
+    <div
+      className="w-100 h-100 position-fixed opacity-100 z-3"
+      style={{
+        top: 0,
+        backgroundColor: 'rgba(51, 51, 51, 0.8)',
+      }}
+    >
+      <BounceLoader
+        color="#a7b0ae"
+        size={100}
+        className="position-absolute m-0"
+        style={{
+          top: '40%',
+          left: '50%',
+          transform: 'translate(-50%,-50%)',
+        }}
+      />
     </div>
   ) : null;
 };
